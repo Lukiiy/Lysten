9789dd058613919312edf155f12f3106daa9b948
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-25 11:37:48 -0300

    Fix ScreenEffectRenderMixin

--- src/main/java/me/lukiiy/lysten/mixin/ScreenEffectRenderMixin.java
+++ src/main/java/me/lukiiy/lysten/mixin/ScreenEffectRenderMixin.java
@@ -4,8 +4,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import me.lukiiy.lysten.client.LystenClient;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.world.effect.MobEffects;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ScreenEffectRenderer.class)
 public class ScreenEffectRenderMixin {
-    @Inject(method = "renderFire", at = @At("HEAD"), cancellable = true)
-    private static void lysten$cancelFireOverlay(PoseStack poseStack, MultiBufferSource bufferSource, TextureAtlasSprite sprite, CallbackInfo ci) {
+    @Inject(method = "submitFire", at = @At("HEAD"), cancellable = true)
+    private static void lysten$cancelFireOverlay(PoseStack poseStack, SubmitNodeCollector submitNodeCollector, TextureAtlasSprite sprite, CallbackInfo ci) {
         if (!LystenClient.filteredFireLayer.get()) return;
 
         LocalPlayer player = Minecraft.getInstance().player;
