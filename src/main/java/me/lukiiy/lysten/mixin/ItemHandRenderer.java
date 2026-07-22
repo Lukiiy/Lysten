183489260920e80a14ef730e48b741a99c460459
Lukiiy <lukiiy@proton.me>
2026-07-22 01:30:55 -0300

    Force offhand to render

---
+++ src/main/java/me/lukiiy/lysten/mixin/ItemHandRenderer.java
@@ -0,0 +1,30 @@
+package me.lukiiy.lysten.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemInHandRenderer.class)
+public abstract class ItemHandRenderer {
+    @Inject(method = "submitArmWithItem", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V", shift = At.Shift.AFTER), cancellable = true)
+    private void lysten$forceOffhand(AbstractClientPlayer player, float frameInterp, float xRot, InteractionHand hand, float attack, ItemStack itemStack, float inverseArmHeight, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int lightCoords, CallbackInfo ci) {
+        if (!itemStack.isEmpty() || hand != InteractionHand.OFF_HAND || player.isInvisible()) return;
+
+        renderArm(poseStack, submitNodeCollector, lightCoords, inverseArmHeight, attack, player.getMainArm().getOpposite());
+        poseStack.popPose();
+
+        ci.cancel();
+    }
+
+    @Invoker("renderPlayerArm")
+    protected abstract void renderArm(PoseStack poseStack, SubmitNodeCollector collector, int light, float equippedProgress, float swingProgress, HumanoidArm arm);
+}
